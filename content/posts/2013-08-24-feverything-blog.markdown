d493e3573157317a8db91cec48459aac77ce9cfd
Kyle Machulis <kyle@nonpolynomial.com>
2013-08-25 00:04:06 -0700

    Minimum viable blog

---
+++ content/posts/2013-08-24-feverything-blog.markdown
@@ -0,0 +1,6 @@
+title: FuckEverything Blog
+slug: fuckeverything-blog
+date: 2013-08-24 23:15:29
+tags: site
+
+Making a blog for feverything project posts. Yaaaay.
--- pelicanconf.py
+++ pelicanconf.py
@@ -3,29 +3,56 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'qDot'
-SITENAME = u'FuckEverything Project Blog'
-SITEURL = ''
+SITENAME = u'FuckEverything Blog'
+SITEURL = 'http://blog.feverything.com'
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'US/Pacific'
+
+MARKUP = (('md', 'markdown'))
 
 DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+FEED_ATOM = ('atom.xml')
 CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
+FEED_ALL_ATOM = None
+FEED_DOMAIN = SITEURL
+TRANSLATION_FEED = None
+FEED_MAX_ITEMS = 10
 
-# Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+# THEME = "themes/slashdong"
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# Relative to content dir
+PAGE_DIR = 'pages'
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
+ARTICLE_DIR = 'posts'
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+CATEGORY_SAVE_AS = False
+TAG_URL = 'tag/{slug}/index.html'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
+AUTHOR_SAVE_AS = False
+
+DAY_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/index.html'
+MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
+YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
 
 DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
+
+PLUGIN_PATH = 'plugins'
+PLUGINS = ['summary', 'neighbors', ]
+
+SUMMARY_END_MARKER = "<!--more-->"
+SUMMARY_MAX_LENGTH = None
+
+FILES_TO_COPY = (('extras/robots.txt', 'robots.txt'),
+                 ('extras/favicon.ico', 'favicon.ico'),
+                 ('extras/htaccess', '.htaccess'))
+
+TEMPLATE_PAGES = {'templates/404.html': "404/index.html"}
+
+MD_EXTENSIONS = ['codehilite', 'extra', 'video']
